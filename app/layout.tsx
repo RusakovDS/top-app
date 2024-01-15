717273723a528870bf09d9191846e5b03d8c7514
Dmitry Rusakov <rusakovtt@yandex.ru>
2024-01-15 18:45:33 +0600

    add font and color palette

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const noto_sans = Noto_Sans({
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["cyrillic"],
+});
 
 export const metadata: Metadata = {
-  title: 'Top App',
-  description: 'Best top app',
-}
+  title: "Top App",
+  description: "Best top app",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={noto_sans.className}>{children}</body>
     </html>
-  )
+  );
 }
