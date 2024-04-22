22dc85c77e395c793e932852ba969d0d4c489448
Dmitry Rusakov <rusakovtt@yandex.ru>
2024-04-22 11:13:22 +0300

    add interfaces

---
+++ interfaces/page.interface.ts
@@ -0,0 +1,42 @@
+export enum TopLevelCategory {
+  Courses,
+  Services,
+  Books,
+  Products
+}
+
+export interface TopPageModel {
+  _id: string;
+  tags: string[];
+  secondCategory: string;
+  alias: string;
+  title: string;
+  category: string;
+  seoText: string;
+  tagsTitle: string;
+  metaTitle: string;
+  metaDescription: string;
+  firstCategory: TopLevelCategory;
+  advantages: TopPageAdvantage[];
+  createdAt: string;
+  updatedAt: string;
+  hh: HhData;
+  qas: any[];
+  addresses: any[];
+  categoryOn: string;
+}
+
+export interface HhData {
+  count: number;
+  juniorSalary: number;
+  middleSalary: number;
+  seniorSalary: number;
+  updatedAt: string;
+  _id: string;
+}
+
+export interface TopPageAdvantage {
+  title: string;
+  description: string;
+  _id: string;
+}
---
+++ interfaces/product.interface.ts
@@ -0,0 +1,46 @@
+export interface ProductModel {
+  _id: string;
+  categories: string[];
+  tags: string[];
+  title: string;
+  image: string;
+  description: string;
+  link: string;
+  price: number;
+  credit: number;
+  oldPrice: number;
+  characteristics: ProductCharacteristic[];
+  advantages?: string;
+  initialRating: number;
+  createdAt: string;
+  updatedAt: string;
+  html: string;
+  blog: Blog;
+  companyId: string;
+  clicks: number;
+  reviews: ReviewModel[];
+  reviewCount: number;
+  reviewAvg?: number;
+}
+
+export interface ReviewModel {
+  _id: string;
+  name: string;
+  title: string;
+  description: string;
+  rating: number;
+  productId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Blog {
+  text: string;
+  _id: string;
+  bigImage?: string;
+}
+
+export interface ProductCharacteristic {
+  name: string;
+  value: string;
+}
